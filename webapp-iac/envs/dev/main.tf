8edc3adb94515d6a258abacc16dffca0f09bee24
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 19:32:29 +0530

    Update main.tf

--- webapp-iac/envs/dev/main.tf
+++ webapp-iac/envs/dev/main.tf
@@ -1,13 +1,26 @@
-module "web_app" {
-  source = "../../modules/web_app"
+terraform {}
 
-  project          = var.project
-  environment      = var.environment
-  owner            = var.owner
+module "web" {
+  source           = "../../modules/web_app"
+  project          = "webapp"
+  environment      = "dev"
+  owner            = "platform"
+
   region           = var.region
   vpc_id           = var.vpc_id
   public_subnet_id = var.public_subnet_id
   instance_type    = var.instance_type
   ssh_cidr_blocks  = var.ssh_cidr_blocks
   extra_tags       = var.extra_tags
+}
+
+variable "region"           { type = string }
+variable "vpc_id"           { type = string }
+variable "public_subnet_id" { type = string }
+variable "instance_type"    { type = string }
+variable "ssh_cidr_blocks"  { type = list(string) }
+variable "extra_tags"       { type = map(string) default = {} }
+
+output "web_public_ip" {
+  value = module.web.web_public_ip
 }
