de9b30d481d08f3a8e65a295a97dfc496f354f7d
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 21:53:05 +0530

    Update main.tf

--- webapp-iac/envs/prod/main.tf
+++ webapp-iac/envs/prod/main.tf
@@ -1,15 +1,31 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 module "web" {
-  source           = "../../modules/web_app"
+  source = "../../modules/web_app"
+
   project          = var.project
   environment      = var.environment
   owner            = var.owner
-  instance_type    = var.instance_type
+
+  region           = var.region              # ✅ required
   vpc_id           = var.vpc_id
   public_subnet_id = var.public_subnet_id
+  instance_type    = var.instance_type
   ssh_cidr_blocks  = var.ssh_cidr_blocks
 }
 
 output "web_public_ip" {
   value = module.web.web_public_ip
 }
-
