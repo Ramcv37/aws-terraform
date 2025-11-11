28240dedc8398ceff625f1800bdf38ee61cd63e3
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 21:43:57 +0530

    Update main.tf

--- webapp-iac/envs/staging/main.tf
+++ webapp-iac/envs/staging/main.tf
@@ -3,12 +3,11 @@ module "web" {
   project          = var.project
   environment      = var.environment
   owner            = var.owner
-  instance_type    = var.instance_type
+
+  region           = var.region              # ✅ required
   vpc_id           = var.vpc_id
   public_subnet_id = var.public_subnet_id
+  instance_type    = var.instance_type
   ssh_cidr_blocks  = var.ssh_cidr_blocks
-}
-
-output "web_public_ip" {
-  value = module.web.web_public_ip
+  extra_tags       = var.extra_tags
 }
