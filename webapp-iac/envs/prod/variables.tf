efa5f121764653b819a6cffd54268c9e30164bed
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 22:04:18 +0530

    Update variables.tf

--- webapp-iac/envs/prod/variables.tf
+++ webapp-iac/envs/prod/variables.tf
@@ -1,33 +1,41 @@
 variable "region" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "project" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "environment" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "owner" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "vpc_id" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "public_subnet_id" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
+  default = ""
 }
 
 variable "ssh_cidr_blocks" {
-  type = list(string)
+  type    = list(string)
+  default = []
 }
 
 variable "extra_tags" {
