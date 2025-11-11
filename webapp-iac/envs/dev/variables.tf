80cbd6b3c81b4ea0c6914fe1f07bb905053eec2d
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 18:48:36 +0530

    Create variables.tf

---
+++ webapp-iac/envs/dev/variables.tf
@@ -0,0 +1,24 @@
+variable "region" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "ssh_cidr_blocks" {
+  type = list(string)
+}
+
+variable "extra_tags" {
+  type    = map(string)
+  default = {}
+}
