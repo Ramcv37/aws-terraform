03ce9a49c1dc85c79f6848aa3645024a7f2da082
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 21:32:56 +0530

    Update main.tf

--- webapp-iac/envs/staging/main.tf
+++ webapp-iac/envs/staging/main.tf
@@ -1,2 +1,14 @@
-# main.tf
-# Calls the web_app module for staging environment
+module "web" {
+  source           = "../../modules/web_app"
+  project          = var.project
+  environment      = var.environment
+  owner            = var.owner
+  instance_type    = var.instance_type
+  vpc_id           = var.vpc_id
+  public_subnet_id = var.public_subnet_id
+  ssh_cidr_blocks  = var.ssh_cidr_blocks
+}
+
+output "web_public_ip" {
+  value = module.web.web_public_ip
+}
