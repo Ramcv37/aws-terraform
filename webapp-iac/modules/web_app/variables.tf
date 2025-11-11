a7d1c4588622c67d0884978f20dfe894542d2446
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 19:37:26 +0530

    Update variables.tf

--- webapp-iac/modules/web_app/variables.tf
+++ webapp-iac/modules/web_app/variables.tf
@@ -1,16 +1,9 @@
-variable "project" { type = string }
-variable "environment" { type = string }
-variable "owner" { type = string }
-variable "region" { type = string }
-variable "vpc_id" { type = string }
+variable "project"          { type = string }
+variable "environment"      { type = string }
+variable "owner"            { type = string }
+variable "region"           { type = string }
+variable "vpc_id"           { type = string }
 variable "public_subnet_id" { type = string }
-variable "instance_type" { type = string }
-
-variable "ssh_cidr_blocks" {
-  type = list(string)
-}
-
-variable "extra_tags" {
-  type    = map(string)
-  default = {}
-}
+variable "instance_type"    { type = string }
+variable "ssh_cidr_blocks"  { type = list(string) }
+variable "extra_tags"       { type = map(string) default = {} }
