cfa95036eaf94d5a4a6f129c09e8b8c8446550ba
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 19:02:11 +0530

    Update variables.tf

--- webapp-iac/modules/web_app/variables.tf
+++ webapp-iac/modules/web_app/variables.tf
@@ -1,11 +1,9 @@
-variable "project"         { type = string }
-variable "environment"     { type = string }
-variable "owner"           { type = string }
-variable "region"          { type = string }
-variable "instance_type"   { type = string  default = "t3.micro" }
-variable "vpc_id"          { type = string }
-variable "public_subnet_id"{ type = string }
-variable "ssh_cidr_blocks" { type = list(string) default = [] }
-variable "extra_tags"      { type = map(string) default = {} }
-# variables.tf
-# Define input variables for the web app module
+variable "project"          { type = string }
+variable "environment"      { type = string }
+variable "owner"            { type = string }
+variable "region"           { type = string }
+variable "vpc_id"           { type = string }
+variable "public_subnet_id" { type = string }
+variable "instance_type"    { type = string }
+variable "ssh_cidr_blocks"  { type = list(string) }
+variable "extra_tags"       { type = map(string) default = {} }
