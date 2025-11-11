e564effd608e314e3e9711dbb3e01dd3951868bd
Ramcv37 <137263540+Ramcv37@users.noreply.github.com>
2025-11-11 19:14:41 +0530

    Update main.tf

--- webapp-iac/modules/web_app/main.tf
+++ webapp-iac/modules/web_app/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical Ubuntu
+  owners      = ["099720109477"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
@@ -64,9 +64,7 @@ resource "aws_instance" "web" {
   subnet_id              = var.public_subnet_id
   vpc_security_group_ids = [aws_security_group.web.id]
 
-  tags = merge(local.tags, {
-    Name = "${var.project}-${var.environment}-web"
-  })
+  tags = merge(local.tags, { Name = "${var.project}-${var.environment}-web" })
 }
 
 output "web_public_ip" {
